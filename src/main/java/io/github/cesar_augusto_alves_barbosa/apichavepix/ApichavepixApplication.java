093e4f16779037a006e46b02928338d2a5f89262
Cesar Augusto <cesar.guga2013@gmail.com>
2025-03-10 10:59:19 -0300

    Iniciando projeto

---
+++ src/main/java/io/github/cesar_augusto_alves_barbosa/apichavepix/ApichavepixApplication.java
@@ -0,0 +1,13 @@
+package io.github.cesar_augusto_alves_barbosa.apichavepix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApichavepixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApichavepixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/cesar_augusto_alves_barbosa/apichavepix/ApichavepixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cesar_augusto_alves_barbosa.apichavepix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApichavepixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
