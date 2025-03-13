88dfbee9fa0bb8c232769970fd9a455bb13d9d0a
Cesar Augusto <cesar.guga2013@gmail.com>
2025-03-12 23:43:19 -0300

    Adicionando mensagem de erro na dto de criação

--- src/main/java/io/github/cesar_augusto_alves_barbosa/apichavepix/dto/PixChaveCriacaoDTO.java
+++ src/main/java/io/github/cesar_augusto_alves_barbosa/apichavepix/dto/PixChaveCriacaoDTO.java
@@ -10,24 +10,25 @@ public record PixChaveCriacaoDTO(
         TipoChave tipoChave,
 
         @NotBlank(message = "O valor da chave não pode estar em branco")
-        @Size(max = 77)
+        @Size(max = 77, message = "O valor da chave deve ter no máximo 77 caracteres")
         String valorChave,
 
         @NotNull(message = "O tipo da conta é obrigatório")
         TipoConta tipoConta,
 
         @NotNull(message = "O número da agência é obrigatório")
-        @Digits(integer = 4, fraction = 0)
+        @Digits(integer = 4, fraction = 0, message = "A agência deve ter até 4 dígitos")
         Integer numeroAgencia,
 
         @NotNull(message = "O número da conta é obrigatório")
-        @Digits(integer = 8, fraction = 0) Integer numeroConta,
+        @Digits(integer = 8, fraction = 0, message = "A conta deve ter até 8 dígitos")
+        Integer numeroConta,
 
         @NotBlank(message = "O nome do correntista é obrigatório")
-        @Size(max = 30)
+        @Size(max = 30, message = "O nome do correntista deve ter no máximo 30 caracteres")
         String nomeCorrentista,
 
-        @Size(max = 45)
+        @Size(max = 45, message = "O sobrenome do correntista deve ter no máximo 45 caracteres")
         String sobrenomeCorrentista,
 
         @NotNull(message = "O tipo do titular é obrigatório")
